1ab971ec93764ef0b4633aa9792149fb56be8939
Michael Schienbein <moomba0@gmail.com>
2021-09-26 14:25:09 +0700

    cleaned up Video component code

--- client/src/video/Video.js
+++ client/src/video/Video.js
@@ -1,48 +1,35 @@
 // import './Youtube.css';
 import YouTube from "react-youtube";
 import { useState, useEffect, useRef } from "react";
-import useDidMountEffect from '../hooks/useDidMountEffect';
+
 
 const Video = ({ sendJsonMessage, globalPlaybackTime }) => {
+  const [playbackTime, setPlaybackTime] = useState(null);
   const [player, setPlayer] = useState(null);
   const [videoId, setVideoId] = useState("M7lc1UVf-VE")
-  const [playbackTime, setPlaybackTime] = useState(null)
-  const onReady = (e) => {
-    console.log(`YouTube Player object for videoId: "${videoId}" has been saved to state.`);
-    setPlayer(e.target);
-
-    // console.debug(player.da)
-    console.debug(player, 'THIS IS THE PLAYER');
-  }
-  // useEffect(() => {
-  //   if (player !== null) {
-  //     player.seekTo(globalPlaybackTime);
-  //   };
-  // }, [globalPlaybackTime])
-
-  // useDidMountEffect(() => {
-  //   // react please run me if 'key' changes, but not on initial render
-  //   console.log("updated player target");
-  // }, [player]);
-  // if (videoUrl) {
-  //   videoCode = videoUrl.split("v=")[1].split("&")[0];
-  // }
-
   const [sequence, setSequence] = useState([]);
   const [timer, setTimer] = useState(null);
+  const onReady = (e) => {
+    console.debug(`YouTube Player object has been saved to state.`);
+    setPlayer(e.target);
+  };
+
+  const onStateChange = (e) => {
+    console.debug(player);
+    handleStateChange(e)
+
+    
+
+  };
+
   const handleStateChange = (e) => handleEvent(e.data);
   const handlePlay = () => console.log("Play!");
   const handlePause = () => console.log("Pause!");
   const handleBuffer = () => console.log("Buffer!");
   const handleSeek = () => {
     console.log("Seek!")
-    console.debug(globalPlaybackTime)
     setPlaybackTime(player.getCurrentTime());
-    // globalPlaybackTime.current = playbackPosition;
-    // console.log(globalPlaybackTime, playbackPosition);
     sendJsonMessage({ type: "time", time: playbackTime });
-    console.debug(globalPlaybackTime, 'THIS IS THE PLAYER TIME');
-    console.debug(playbackTime, 'this is the local time')
   };
 
   const isSubArrayEnd = (A, B) => {
@@ -76,59 +63,16 @@ const Video = ({ sendJsonMessage, globalPlaybackTime }) => {
     }
   };
 
-  const onStateChange = (e) => {
-
-
-    handleStateChange(e)
-    // console.log(e.target.playerInfo);
-
-    // e.target.pauseVideo();
-    // console.log(currentTime);
-    // playbackPosition.current = globalPlaybackTime;
-    // console.debug(playbackPosition, "LOOK")
-    // console.debug(player, 'THIS IS THE PLAYER');
-    // console.debug(player.getPlayerState(), 'THIS IS THE PLAYER');
-    
-
-
-    // if (player.playerInfo.playerState === 3) {
-    //   player.pauseVideo();
-    //   player.playVideo();
-    // };
-    // console.debug(player,'THIS IS THE PLAYER');
-    // setStateChange(stateChange === true ? false : true);
-    // console.log(stateChange);
-    // sendJsonMessage({ type: "videoId", videoId, action: "add" });
-    // if (player !== null) {
-    //       timer.current = setTimeout(() => {
-    //         player.pauseVideo();
-    //         // player.seekTo(globalPlaybackTime);
-    //         // player.playVideo();
-    //       }, 1000);
-    //     }
-    //     console.log('effect');
-    //     return () => {
-    //       clearTimeout(timer.current);
-    //     };
-    // e.target.seekTo(globalPlaybackTime);
-    // const duration = e.target.getDuration();
-  };
-  // const sync = () => {
-  //   // console.log('ready')
-  // }
   const opts = {
-    // height: '640',
-    // width: '1137.78',
-    height: '100',
-    width: '200',
+
+    height: '200',
+    width: '400',
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
     },
   };
   return (
     <div>
-      {/* <input value={videoUrl} onChange={(e) => setVideoUrl(e.target.value)} /> */}
       <YouTube
         videoId={videoId}
         containerClassName="Youtube"
@@ -136,7 +80,6 @@ const Video = ({ sendJsonMessage, globalPlaybackTime }) => {
         onReady={onReady}
         opts={opts}
       />
-      {/* <button onClick={sync}>SYNC</button> */}
     </div>
   );
 };
