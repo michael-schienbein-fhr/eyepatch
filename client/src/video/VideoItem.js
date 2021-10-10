f2ddf67e50a066261c90476283938cf39d9287fb
Michael Schienbein <moomba0@gmail.com>
2021-10-10 18:46:19 +0700

    styles

--- client/src/video/VideoItem.js
+++ client/src/video/VideoItem.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import './video-item.css';
 
-const VideoItem = ({video , handleVideoQueing}) => {
+const VideoItem = ({ video, handleVideoQueing }) => {
     return (
-        <div onClick={ () => handleVideoQueing(video)} className=' video-item item'>
-            <img className='image' src={video.snippet.thumbnails.medium.url} alt={video.snippet.description}/>
-            {/* <div className='content'>
-                <div className='header '>{video.snippet.title}</div>
-            </div> */}
-        </div>
+            <div onClick={() => handleVideoQueing(video)} className=' video-item item'>
+                <img className='image' src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
+            </div>
     )
 };
 export default VideoItem;
+
+// add video title on hover
--- client/src/video/VideoItem2.js
+++ client/src/video/VideoItem2.js
@@ -4,14 +4,28 @@ import './video-item.css';
 const VideoItem = ({ video, handleVideoRemoval, handleVideoChange }) => {
   return (
     <>
-      <div onClick={() => handleVideoRemoval(video)}>X</div>
-      <div onClick={() => handleVideoChange(video)} className='video-item item'>
-        <img className='image' src={video.thumbnail} alt={video.description} />
-        {/* <div className='content'>
-          <div className='header '>{video.title}</div>
-        </div> */}
+      <div class='video-item-container'>
+        <div href="#" class="like-button" title="Like Button">
+          <i class="fa fa-heart fa-1x"></i>
+        </div>
+        <div onClick={() => handleVideoChange(video)} className='video-item item'>
+          <div class="video-item-remove" onClick={() => handleVideoRemoval(video)}>X</div>
+          <img className='image' src={video.thumbnail} alt={video.description} />
+        </div>
       </div>
     </>
   )
 };
 export default VideoItem;
+
+{/* <div class="container">
+  <div href="#" class="like-button" title="Like Button">
+    <i class="fa fa-heart fa-1x"></i>
+  </div>
+  <img src="https://staticvideos.pexels.com/videos/1448735/pictures/preview-0.jpg" alt="Beautiful Image" class="image">
+  <div class ="overlay">
+    <button href="#" class ="play-icon" title="Video Play">
+    <i class ="fa fa-play"></i>
+  </button>
+  </div>
+</div> */}
